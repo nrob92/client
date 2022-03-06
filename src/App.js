11a9c4fa4f528342d89c2e0dcf517068bbc54fe2
nrob92 <74805095+nrob92@users.noreply.github.com>
2022-03-06 17:40:25 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [foodName, setFoodName] = useState("");
+  const [days, setDays] = useState(0);
+  const [foodList, setFoodList] = useState([]);
+  const [newFoodName, setNewFoodName] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setFoodList(response.data);
+    });
+  }, []);
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      foodName,
+      days,
+    });
+  };
+
+  const updateFood = (id) => {
+    Axios.put("http://localhost:3001/update", {
+      id,
+      newFoodName,
+    });
+  };
+
+  const deleteFood = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Crud App With MERN</h1>
+      <label>Food Name</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFoodName(e.target.value);
+        }}
+      />
+      <label>Days Since You Ate It</label>
+      <input
+        type="number"
+        onChange={(e) => {
+          setDays(e.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add to List</button>
+
+      <hr />
+      {foodList.map((value, key) => {
+        return (
+          <div className="food" key={key}>
+            <h1>{value.foodName}</h1>
+            <h1>{value.daysSinceIAte}</h1>
+            <input
+              type="text"
+              placeholder="New Food Name"
+              onChange={(e) => {
+                setNewFoodName(e.target.value);
+              }}
+            />
+            <button onClick={() => updateFood(value._id)}>Update</button>
+            <button onClick={() => deleteFood(value._id)}>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
